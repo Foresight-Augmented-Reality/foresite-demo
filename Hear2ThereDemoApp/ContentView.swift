4bcaf91ca9beb9908ffec32e178f57faab712760
Clark Rinker <cgrinker@gmail.com>
2025-02-10 10:47:57 -0800

    Initial Commit

---
+++ Hear2ThereDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hear2ThereDemoApp
+//
+//  Created by clark rinker on 2/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hear2ThereDemoApp/Hear2ThereDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hear2ThereDemoAppApp.swift
+//  Hear2ThereDemoApp
+//
+//  Created by clark rinker on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Hear2ThereDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
